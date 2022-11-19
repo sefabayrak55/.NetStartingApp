c77b47c340485f9ae682d17f6d4172c0308cc4d6
bayrak.sefa <sefabayrak55@hotmail.com>
2022-11-19 16:49:57 +0300

    .Net MVC Starting App

---
+++ ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        string message = "Merhaba Models-Views-Controllers";
+        return View("Index", message);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult GetProduct()
+    {
+        var product = new Product();
+        product.Id = 1;
+        product.productName = "Computer";
+        product.price = 15000;
+
+        return View("GetProduct", product);
+    }
+
+    public IActionResult GetProducts()
+    {
+        var productList = new List<Product>()
+        {
+            new Product(1,"Computer",15000),
+            new Product(2, "iPhone", 42500)
+        };
+        return View(productList);
+    }
+}
+
---
+++ ProductApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ProductApp.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string productName { get; set; }
+
+		public decimal price { get; set; }
+
+        public Product(int id, string productName, decimal price)
+        {
+            Id = id;
+            this.productName = productName;
+            this.price = price;
+        }
+
+        public Product()
+		{
+
+		}
+	}
+}
+
